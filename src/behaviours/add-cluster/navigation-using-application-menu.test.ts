e7daf506036cf32eb21a0b345dd3779af86bfa7f
Janne Savolainen <janne.savolainen@live.fi>
2022-05-11 08:13:40 +0300

    Extract updating is enabled to separate injectable
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/behaviours/add-cluster/navigation-using-application-menu.test.ts
+++ src/behaviours/add-cluster/navigation-using-application-menu.test.ts
@@ -6,7 +6,6 @@
 import type { RenderResult } from "@testing-library/react";
 import type { ApplicationBuilder } from "../../renderer/components/test-utils/get-application-builder";
 import { getApplicationBuilder } from "../../renderer/components/test-utils/get-application-builder";
-import isAutoUpdateEnabledInjectable from "../../main/update-app/is-auto-update-enabled.injectable";
 
 // TODO: Make components free of side effects by making them deterministic
 jest.mock("../../renderer/components/tooltip");
@@ -17,9 +16,7 @@ describe("add-cluster - navigation using application menu", () => {
   let rendered: RenderResult;
 
   beforeEach(async () => {
-    applicationBuilder = getApplicationBuilder().beforeApplicationStart(({ mainDi }) => {
-      mainDi.override(isAutoUpdateEnabledInjectable, () => () => false);
-    });
+    applicationBuilder = getApplicationBuilder();
 
     rendered = await applicationBuilder.render();
   });
